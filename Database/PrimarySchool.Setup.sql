a9e62069a1da34e6babacd1de701660ddd2573ad
Antoine Raquillet <antoine.raquillet@esiea.fr>
2018-09-24 14:47:40 +0200

    Added ps.vTeacher.

--- Database/PrimarySchool.Setup.sql
+++ Database/PrimarySchool.Setup.sql
@@ -13,6 +13,13 @@ begin
 end;
 GO
 
+if exists(select * from sys.views t
+              inner join sys.schemas s on s.schema_id = t.schema_id
+          where t.[name] = 'vTeacher' and s.[name] = 'ps')
+begin
+	drop view ps.vTeacher;
+end;
+
 if exists(select * from sys.tables t
               inner join sys.schemas s on s.schema_id = t.schema_id
           where t.[name] = 'tStudent' and s.[name] = 'ps')
@@ -82,3 +89,17 @@ create table ps.tStudent
 );
 
 insert into ps.tStudent(FirstName, LastName, BirthDate, ClassId) values(N'N.A.', N'N.A.', '00010101', 0);
+GO
+
+create view ps.vTeacher
+as
+	select
+		TeacherId = t.TeacherId,
+		FirstName = t.FirstName,
+		LastName = t.LastName,
+		ClassId = coalesce(c.ClassId, 0),
+		ClassName = coalesce(c.[Name], N''),
+		ClassLevel = coalesce(c.[Level], '')
+	from ps.tTeacher t
+		left outer join ps.tClass c on c.TeacherId = t.TeacherId
+	where t.TeacherId <> 0;
